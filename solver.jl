0a115bb17aa6aca5429794904f2ba3f4d22fc5ae
jinbeizame007 <jinbeizame007@gmail.com>
2017-09-11 16:32:23 +0900

    no errorFlag

--- solver.jl
+++ solver.jl
@@ -6,18 +6,14 @@ const bx = 3
 const by = 3
 const box_size = bx * by
 const cell_size = box_size * box_size
-#bx = 3
-#by = 3
-#box_size = bx * by
-#cell_size = box_size * box_size
 global updateFlag = [false]
+global errorFlag = [false]
 
 type Solver
     puzzle::Array{Int64,2}
-    candidates::Array{Array{Array{Bool,1},1},1}
+    candidates::Array{Int64,3}
     function Solver()
-        cand = [[[true for i in 1:box_size] for j in 1:box_size] for l in 1:box_size]
-        new(zeros(Int64,(9,9)),cand)
+        new(zeros(Int64,(box_size,box_size)),ones(Int64,(box_size,box_size,box_size)))
     end
 end
 function ToStringFromPuzzle(puz::Array{Int64,2})
@@ -37,7 +33,7 @@ function ToStringFromPuzzle(puz::Array{Int64,2})
     end
     println(par)
 end
-function ToStringFromCandidates(cand::Array{Array{Array{Bool,1},1},1})
+function ToStringFromCandidates(cand::Array{Int64,3})
     for y in 1:box_size
         for y_ in 1:by
             for x in 1:box_size
@@ -45,7 +41,7 @@ function ToStringFromCandidates(cand::Array{Array{Array{Bool,1},1},1})
                     print(" ")
                 end
                 for x_ in 1:bx
-                    if cand[y][x][(y_-1)*bx+x_] == true
+                    if cand[y,x,(y_-1)*bx+x_] == 1
                         print((y_-1)*bx+x_)
                     else
                         print(".")
@@ -57,35 +53,51 @@ function ToStringFromCandidates(cand::Array{Array{Array{Bool,1},1},1})
         println("")
     end
 end
-function DeleteCandidate(candidates::Array{Array{Array{Bool,1},1},1},x::Int64,y::Int64,n::Int64)
-    candidates[y][x][n] = false
-end
-function PutNumber(puzzle::Array{Int64,2},candidates::Array{Array{Array{Bool,1},1},1},x::Int64,y::Int64,n::Int64)
-    if candidates[y][x][n] == false
-        #println("false")
+function DeleteCandidate(puzzle::Array{Int64,2},candidates::Array{Int64,3},y::Int64,x::Int64,n::Int64)
+    if puzzle[y,x] != 0
         return
     end
-    puzzle[y,x] = n
-    px = div(x-1,bx) * bx + 1
-    py = div(y-1,by) * by + 1
+    candidates[y,x,n] = 0
+    count = 0
     for i in 1:box_size
-        candidates[y][x][i] = false
-        candidates[y][i][n] = false
-        candidates[i][x][n] = false
-        candidates[py+div(i-1,bx)][px+mod(i-1,bx)][n] = false
+        if candidates[y,x,i] == 1
+            count += 1
+        end
+    end
+    if count == 0
+        #errorFlag[1] = true
     end
 end
-function SetPuzzle(puzzle::Array{Int64,2},candidates::Array{Array{Array{Bool,1},1},1},puz::Array{Int64,2})
-    """
+function PutNumber(puzzle::Array{Int64,2},candidates::Array{Int64,3},x::Int64,y::Int64,n::Int64)
+    if candidates[y,x,n] == 0
+        #errorFlag[1] = true
+        return
+    end
+    for i in 1:box_size
+        candidates[y,x,i] = 0
+    end
+    candidates[y,x,n] = 1
+    puzzle[y,x] = n
+    px = (x-1)÷bx * bx + 1
+    py = (y-1)÷by * by + 1
+    for i in 1:box_size
+        DeleteCandidate(puzzle,candidates,y,x,i)
+        DeleteCandidate(puzzle,candidates,y,i,n)
+        DeleteCandidate(puzzle,candidates,i,x,n)
+        DeleteCandidate(puzzle,candidates,py+(i-1)÷bx,px+(i-1)%bx,n)
+    end
+end
+function SetPuzzle(puzzle::Array{Int64,2},candidates::Array{Int64,3},puz::Array{Int64,2})
     for y in 1:box_size
         for x in 1:box_size
             puzzle[y,x] = 0
             for n in 1:box_size
-                candidates[y][x][n] = true
+                candidates[y,x,n] = 1
             end
         end
     end
-    """
+    #puzzle = zeros(Int64,(box_size,box_size))
+    #candidates = ones(Int64,(box_size,box_size,box_size))
     for y in 1:box_size
         for x in 1:box_size
             if puz[y,x] != 0
@@ -94,20 +106,34 @@ function SetPuzzle(puzzle::Array{Int64,2},candidates::Array{Array{Array{Bool,1},
         end
     end
 end
-function Singles(puzzle::Array{Int64,2},candidates::Array{Array{Array{Bool,1},1},1})
+function Singles(puzzle::Array{Int64,2},candidates::Array{Int64,3})
+    if errorFlag[1]
+        return
+    end
     n2 = 0
     for y in 1:box_size
         for x in 1:box_size
             count = 0
-            for cand in candidates[y][x]
-                if cand == true
+            if puzzle[y,x] != 0
+                continue
+            end
+            for n in 1:box_size
+                if candidates[y,x,n] == 1
                     count += 1
+                    if count>1
+                        break
+                    end
+                    n2 = n
                 end
             end
             if count != 1
                 continue
             end
-            PutNumber(puzzle,candidates,x,y,findfirst(candidates[y][x],true))
+            PutNumber(puzzle,candidates,x,y,n2)#findfirst(candidates[y,x],1))
+            if errorFlag[1] == true
+                updateFlag[1] = false
+                return
+            end
             updateFlag[1] = true
         end
     end
@@ -124,31 +150,41 @@ function Singles(puzzle::Array{Int64,2},candidates::Array{Array{Array{Bool,1},1}
             end
             #count = zeros(Int64,(3,2))#[[],[],[]]
             #count2 = zeros(Int64,3)
-            px = mod(i-1,bx) * bx + 1
-            py = div(i-1,bx) * by + 1
+            px = (i-1)%bx * bx + 1
+            py = (i-1)÷bx * by + 1
+
             for j in 1:box_size
-                if candidates[i][j][n] == true
-                    #append!(count[1],[j,i])
+                if count2[1] < 2 && candidates[i,j,n] == 1
                     count[1,1] = j
                     count[1,2] = i
                     count2[1] += 1
                 end
-                if candidates[j][i][n] == true
-                    #append!(count[2],[i,j])
+                if count2[2] < 2 && candidates[j,i,n] == 1
                     count[2,1] = i
                     count[2,2] = j
                     count2[2] += 1
                 end
-                if candidates[py+div(j-1,bx)][px+mod(j-1,bx)][n] == true
-                    #append!(count[3],[px+mod(j-1,bx),py+div(j-1,bx)])
-                    count[3,1] = px+mod(j-1,bx)
-                    count[3,2] = py+div(j-1,bx)
+                if count2[3] < 2 && candidates[py+(j-1)÷bx,px+(j-1)%bx,n] == 1
+                    count[3,1] = px+(j-1)%bx
+                    count[3,2] = py+(j-1)÷bx
                     count2[3] += 1
+                end
+                if count2[1]>=2 && count2[2]>=2 && count2[3]>=2
+                    break
                 end
             end
             for j in 1:3
-                if count2[j] == 1
+                if count2[j] == 0
+                    updateFlag[1] = false
+                    #errorFlag[1] = true
+                    return
+                end
+                if count2[j] == 1 && puzzle[count[j,2],count[j,1]] == 0
                     PutNumber(puzzle,candidates,count[j,1],count[j,2],n)
+                    if errorFlag[1] == true
+                        updateFlag[1] = false
+                        return
+                    end
                     updateFlag[1] = true
                 end
             end
@@ -159,14 +195,14 @@ function Solve(solver::Solver)
     while true
         updateFlag[1] = false
         Singles(solver.puzzle,solver.candidates)
-        #ToStringFromPuzzle(solver.puzzle)
-        #println(updateFlag[1][1])
+        if errorFlag[1]
+            return false
+        end
         if updateFlag[1]
             continue
         end
         break
     end
-    #ToStringFromPuzzle(solver.puzzle)
     for y in 1:box_size
         for x in 1:box_size
             if solver.puzzle[y,x] == 0
@@ -176,14 +212,16 @@ function Solve(solver::Solver)
     end
     return true
 end
-function CalcScore(solver::Solver)
-    #ToStringFromPuzzle(solver.puzzle)
+function CalcScore(candidates::Array{Int64,3})
+    if errorFlag[1]
+        return 10000000
+    end
     score = 0
-    for cand in solver.candidates
-        for can in cand
+    for y in 1:box_size
+        for x in 1:box_size
             score_ = 0
-            for c in can
-                if c == true
+            for n in 1:box_size
+                if candidates[y,x,n] == 1
                     score_ += 1
                 end
             end
@@ -192,6 +230,69 @@ function CalcScore(solver::Solver)
     end
     return score
 end
+"""
+mutable struct Generator
+    puzzle::Array{Int64,2}
+    score::Int64
+    solver::Solver
+    scoreFlag::Bool
+    function Generator()
+        new(zeros(Int64,(9,9)),100000,Solver(),false)
+    end
+end
+function CheckRule(puzzle::Array{Int64,2},cand::Array{Int64,3},x::Int64,y::Int64,n::Int64)
+    if cand[y,x,n]==1 && puzzle[y,x]==0
+        return true
+    else
+        return false
+    end
+end
+function LocalSearch(generator::Generator)
+    #println("")
+    #ToStringFromPuzzle(generator.solver.puzzle)
+    #ToStringFromPuzzle(generator.puzzle)
+    for y_ in 1:box_size
+        for x_ in 1:box_size
+            puz = deepcopy(generator.solver.puzzle)
+            if puz[y_,x_] == 0
+                continue
+            end
+            n_ = puz[y_,x_]
+            puz[y_,x_] = 0
+            #SetPuzzle(generator.solver.puzzle,generator.solver.candidates,puz)
+            for y in 1:box_size
+                for x in 1:box_size
+                    if puz[y,x] != 0
+                        continue
+                    end
+                    for n in 1:box_size
+                        solver = Solver()
+                        SetPuzzle(solver.puzzle,solver.candidates,puz)
+                        if CheckRule(solver.puzzle,solver.candidates,x,y,n)
+                            puz[y,x] = n
+                            PutNumber(solver.puzzle,solver.candidates,x,y,n)
+                            Solve(solver)
+                            score = CalcScore(solver.candidates)
+                            if score < generator.score
+                                #ToStringFromPuzzle(puz)
+                                generator = Generator()
+                                generator.puzzle = deepcopy(puz)
+                                generator.score = score
+                                SetPuzzle(generator.solver.puzzle,generator.solver.candidates,puz)
+                                generator.scoreFlag = true
+                                return generator
+                            end
+                            puz[y,x] = 0
+                        end
+                    end
+                end
+            end
+            #puz[y_,x_] = n_
+        end
+    end
+    return generator
+end
+"""
 
 mutable struct Generator
     puzzle::Array{Int64,2}
@@ -202,25 +303,32 @@ mutable struct Generator
         new(zeros(Int64,(9,9)),100000,Solver(),false)
     end
 end
-function CheckRule(cand::Array{Array{Array{Bool,1},1},1},x::Int64,y::Int64,n::Int64)
-    return cand[y][x][n]
-    """
-    px = div(x-1,bx) * bx + 1
-    py = div(y-1,by) * by + 1
-    for i in 1:box_size
-        if puz[x,i] == n
-            return false
-        end
-        if puz[i,y] == n
-            return false
-        end
-        if puz[py+div(i-1,bx),px+mod(i-1,bx)] == n
-            return false
+function CheckRule(puzzle::Array{Int64,2},cand::Array{Int64,3},x::Int64,y::Int64,n::Int64)
+    if cand[y,x,n]==1 && puzzle[y,x]==0
+        return true
+    else
+        return false
+    end
+end
+function init_puz(generator::Generator, clue_count::Int64)
+    generator.solver = Solver()
+    #generator.solver.puzzle = zeros(Int64,(9,9))
+    #generator.solver.candidates = ones(Int64,(9,9,9))
+    count = 0
+    while count < clue_count
+        x = rand(1:box_size)
+        y = rand(1:box_size)
+        n = rand(1:box_size)
+        if generator.solver.candidates[y,x,n] == 1 && generator.solver.puzzle[y,x] == 0
+            count += 1
+            PutNumber(generator.solver.puzzle,generator.solver.candidates,x,y,n)
         end
     end
-    return true
-    """
+    generator.puzzle = copy(generator.solver.puzzle)
+    generator.score = CalcScore(generator.solver.candidates)
+    return generator
 end
+"""
 function init_puz(generator::Generator, clue_count::Int64)
     generator.solver.puzzle = zeros(Int64,(9,9))
     count = 0
@@ -228,15 +336,16 @@ function init_puz(generator::Generator, clue_count::Int64)
         x = rand(1:box_size)
         y = rand(1:box_size)
         n = rand(1:box_size)
-        if CheckRule(generator.solver.candidates,x,y,n)
+        if CheckRule(generator.solver.puzzle,generator.solver.candidates,x,y,n)
             count += 1
             PutNumber(generator.solver.puzzle,generator.solver.candidates,x,y,n)
         end
     end
     generator.puzzle = copy(generator.solver.puzzle)
-    generator.score = CalcScore(generator.solver)
+    generator.score = CalcScore(generator.solver.candidates)
     return generator
 end
+"""
 function LocalSearch(generator::Generator)
     puzzle = copy(generator.puzzle)
     for y_ in 1:box_size
@@ -251,17 +360,19 @@ function LocalSearch(generator::Generator)
             for y in 1:box_size
                 for x in 1:box_size
                     for n in 1:box_size
-                        if CheckRule(solver.candidates,x,y,n)
+                        if CheckRule(solver.puzzle,solver.candidates,x,y,n)
                             puz[y,x] = n
-                            generator.solver = Solver()
-                            SetPuzzle(generator.solver.puzzle,generator.solver.candidates,puz)
-                            score = CalcScore(generator.solver)
+                            solver = Solver()
+                            SetPuzzle(solver.puzzle,solver.candidates,puz)
+                            Solve(solver)
+                            score = CalcScore(solver.candidates)
                             if score < generator.score
                                 #prInt64ln(score)
                                 #ToStringFromPuzzle(puz)
+                                generator = Generator()
                                 generator.puzzle = copy(puz)
                                 generator.score = score
-                                SetPuzzle(generator.solver.puzzle,generator.solver.candidates, puz)
+                                SetPuzzle(generator.solver.puzzle,generator.solver.candidates, solver.puzzle)
                                 generator.scoreFlag = true
                                 return generator
                             end
@@ -275,24 +386,25 @@ function LocalSearch(generator::Generator)
     return generator
 end
 function Generate(generator::Generator, clue_count::Int64)
-    generator = init_puz(generator,clue_count)
+    #println("********************")
     while true
+        errorFlag[1] = false
         generator.scoreFlag = false
         generator = LocalSearch(generator)
-        if generator.scoreFlag ==  false
-            generator = Generator()
-            generator = init_puz(generator,clue_count)
-            continue
+        #println(generator.score)
+        if generator.scoreFlag == false
+            break
         end
-        #generator.solver = Solver(3,3)
-    #    generator.solver = SetPuzzle(generator.solver, generator.puzzle)
-        if Solve(generator.solver)
-            ToStringFromPuzzle(generator.puzzle)
-            ToStringFromPuzzle(generator.solver.puzzle)
-            return true
-        end
-        #next!(progress)
     end
+    if Solve(generator.solver)
+        ToStringFromPuzzle(generator.puzzle)
+        ToStringFromPuzzle(generator.solver.puzzle)
+        println(count)
+        return true
+    else
+        return false
+    end
+    #next!(progress)
 end
 
 puz_easy = [0 0 0 0 1 0 0 0 0
@@ -314,10 +426,12 @@ puz_hard = [4 0 0 0 9 0 0 0 7
             0 9 0 2 0 0 0 5 0
             5 0 0 0 4 0 0 0 1]
 
-
 #solver = Solver()
-#SetPuzzle(solver.puzzle,solver.candidates,puz_easy)
-#println(Solve(solver))
+#SetPuzzle(solver.puzzle,solver.candidates,puz_hard)
+#ToStringFromPuzzle(solver.puzzle)
+#Solve(solver)
+#ToStringFromPuzzle(solver.puzzle)
+#ToStringFromCandidates(solver.candidates)
 
 function solve_puz17()
     puz17 = [zeros(Int64,(9,9)) for i in 1:49157]
@@ -340,14 +454,14 @@ function solve_puz17()
         SetPuzzle(solver.puzzle,solver.candidates,puz)
         if Solve(solver)
             count += 1
+            #ToStringFromPuzzle(solver.puzzle)
         end
         #next!(progress)
     end
     println(count)
-
 end
 #Profile.clear()
-#@profile solve_puz17()
+#@profile solve_puz17()##
 #Profile.print(format=:flat)
 #@time solve_puz17()
 
@@ -355,9 +469,28 @@ function test()
     for i in 1:20
         generator = Generator()
         generator = init_puz(generator,21)
-        ToStringFromPuzzle(generator.solver.puzzle)
+        #ToStringFromPuzzle(generator.solver.puzzle)
         Generate(generator,21)
     end
 end
-
 @time test()
+#Profile.clear()
+#@profile test()
+#Profile.print(format=:flat)
+
+#generator = Generator()
+#generator = init_puz(generator,21)
+#@time Generate(generator,21)
+#ToStringFromPuzzle(generator.solver.puzzle)
+#ToStringFromCandidates(generator.solver.candidates)
+#ToStringFromPuzzle(generator.solver.puzzle)
+#Profile.clear()
+count = 0
+
+#while Generate(generator,21) == false
+#    count += 1
+#    generator = Generator()
+#    generator = init_puz(generator,21)
+#end
+#println(count)
+#Profile.print(format=:flat)
